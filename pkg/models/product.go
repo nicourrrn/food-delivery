a6b7fb3906c89c1fcfa90df33d8533e770bb2230
Vadym Sorokin <s57111702@gmail.com>
2022-02-07 19:14:47 +0200

    Add types with methods of types for product, ingredient and supplier

--- pkg/models/product.go
+++ pkg/models/product.go
@@ -1,7 +1,42 @@
 package models
 
-var Ingredients map[int]*string
-var ProductTypes map[int]*string
+import "errors"
+
+type Ingredient *string
+
+var ingredients map[int]Ingredient
+
+func GetIngredientById(id int) (Ingredient, error) {
+	ing, ok := ingredients[id]
+	if !ok {
+		return nil, errors.New("ingredient not found")
+	}
+	return ing, nil
+}
+func UpdateIngredients(newTypes map[int]string) {
+	ingredients = make(map[int]Ingredient)
+	for id, str := range newTypes {
+		ingredients[id] = &str
+	}
+}
+
+type ProductType *string
+
+var productTypes map[int]ProductType
+
+func GetProductType(id int) (ProductType, error) {
+	productType, ok := productTypes[id]
+	if !ok {
+		return nil, errors.New("ingredient not found")
+	}
+	return productType, nil
+}
+func UpdateProductTypes(newTypes map[int]string) {
+	productTypes = make(map[int]ProductType)
+	for id, str := range newTypes {
+		productTypes[id] = &str
+	}
+}
 
 type Product struct {
 	ID                int
@@ -10,5 +45,12 @@ type Product struct {
 	Ingredients       []*string
 	Name, Description string
 	Image             string
-	Type              *string
+	Type              ProductType
+}
+
+func NewProduct(name string, price float32, prodType ProductType) (Product, error) {
+	if prodType == nil {
+		return Product{}, errors.New("product type is nil")
+	}
+	return Product{Name: name, Price: price, Type: prodType}, nil
 }
