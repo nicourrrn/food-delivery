028b60880b20f5f1c574d48d64aae99cbcde11fd
nicourrrn <nicourrrn@gmail.com>
2022-02-11 18:50:52 +0200

    Replace LoadUserById to DB

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -2,7 +2,9 @@ package db
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
+	"food-delivery/pkg/models"
 	"sync"
 )
 
@@ -26,4 +28,26 @@ func NewDB(login, password, dbName string) (DB, error) {
 
 type Garbage interface {
 	GarbageCollector(group sync.WaitGroup)
+}
+
+func (r *DB) LoadUserByID(id int) (models.TypedUser, error) {
+	row := r.Conn.QueryRow("SELECT users.id, users.name, users.login, users.email, ut.name FROM users JOIN users_types ut on users.user_type_id = ut.id WHERE users.id = ?", id)
+	user := models.User{}
+	var userType string
+	err := row.Scan(user.ID, user.Name, user.Login, user.Email, userType)
+	if err != nil {
+		return nil, err
+	}
+	var castedUser models.TypedUser
+	switch userType {
+	case "Supplier":
+		castedUser, err = GlobalSupplierRepo.LoadSupplier(user)
+	case "Branch":
+		castedUser, err = GlobalSupplierRepo.LoadBranch(user)
+	case "Client":
+		castedUser, err = GlobalClientRepo.LoadClient(user)
+	default:
+		err = errors.New("user type not found from db")
+	}
+	return castedUser, err
 }
