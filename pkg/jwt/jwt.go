492123a41eb31ff1e888f2fdfd74042132ff954e
nicourrrn <nicourrrn@gmail.com>
2022-02-21 17:43:59 +0200

    Write jwt token pkg

---
+++ pkg/jwt/jwt.go
@@ -0,0 +1,62 @@
+package jwt
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt"
+	"os"
+	"strconv"
+	"time"
+)
+
+var (
+	accessKey, refreshKey           string
+	accessLifeTime, refreshLifeTime time.Duration
+)
+
+func InitJwt() error {
+	refreshKey = os.Getenv("REFRESH_KEY")
+	accessKey = os.Getenv("ACCESS_KEY")
+	tempInt, err := strconv.Atoi(os.Getenv("ACCESS_LIFETIME_SECOND"))
+	if err != nil {
+		return err
+	}
+	refreshLifeTime = time.Second * time.Duration(tempInt)
+	tempInt, err = strconv.Atoi(os.Getenv("REFRESH_LIFETIME_SECOND"))
+	if err != nil {
+		return err
+	}
+	refreshLifeTime = time.Second * time.Duration(tempInt)
+	return nil
+}
+
+type UserClaim struct {
+	jwt.StandardClaims
+	UserId int64
+}
+
+func NewUserClaim(userId int64, lifeTime time.Time) *UserClaim {
+	return &UserClaim{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: lifeTime.Unix(),
+		},
+		UserId: userId,
+	}
+}
+
+func (c *UserClaim) SetKey(key string) (string, error) {
+	return jwt.NewWithClaims(jwt.SigningMethodHS256, c).SignedString(key)
+}
+
+func GetClaim(token, key string) (*UserClaim, error) {
+	jwtToken, err := jwt.ParseWithClaims(token, &UserClaim{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(key), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	claims, ok := jwtToken.Claims.(*UserClaim)
+	if !ok || !jwtToken.Valid {
+		return nil, errors.New("failed to parse")
+	}
+	return claims, nil
+}
---
+++ pkg/jwt/pairs.go
@@ -0,0 +1,23 @@
+package jwt
+
+import "time"
+
+type TokenPair struct {
+	RefreshToken, AccessToken *UserClaim
+}
+
+func NewTokenPair(userId int64) TokenPair {
+	return TokenPair{
+		RefreshToken: NewUserClaim(userId, time.Now().Add(refreshLifeTime)),
+		AccessToken:  NewUserClaim(userId, time.Now().Add(accessLifeTime)),
+	}
+}
+
+func (p TokenPair) GetStrings() (refresh, access string, err error) {
+	refresh, err = p.RefreshToken.SetKey(refreshKey)
+	if err != nil {
+		return "", "", err
+	}
+	access, err = p.AccessToken.SetKey(accessKey)
+	return
+}
